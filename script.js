3faf68d637a12b55dcd293efbec6c05af941c863
NERD <93147651+Nerd2004@users.noreply.github.com>
2023-01-17 18:15:03 +0530

    Update script.js
    This is the Final Changes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let curr='X';
 let box = document.querySelectorAll('.slot');
 let tur = document.getElementById('turn');
 let reset = document.getElementsByClassName('buttons')[0];
+let count = 0;
 let wins=[
     [0,1,2,0,11.5,0],
     [3,4,5,0,24,0],
@@ -14,6 +15,8 @@ let wins=[
 ]
 reset.onclick = () =>{
     curr ='X';
+    count = 0;
+    variable = "A";
     tur.innerText =` It's ${curr}'s Turn`;
     box.forEach(x => {
             x.innerText="";
@@ -21,8 +24,6 @@ reset.onclick = () =>{
     })
     document.querySelector('.line').style.backgroundColor = "#091B3A";
     document.querySelector('.line').style.transform = `rotate(0) translate(0vh,0vh)`;
-    // document.querySelector('.board').style.background = "#091B3A";
-    // document.querySelector('.game').style.background = "#091B3A";
 }
 draw =(a,b,c,x,y,angle) =>{
     document.querySelector('.line').style.backgroundColor = "white";
@@ -35,21 +36,21 @@ check = () =>{
                 x.disabled = true;
                 variable = box[e[0]].innerText;    
         }) 
+        tur.innerText = `${variable} WON`;
         draw(e[0],e[1],e[2],e[3],e[4],e[5]);
         }
-})
+    })
+    if(count === 9 && variable === "A") tur.innerText="DRAW";
 }
 let variable ="A";
 box.forEach(x => {
     x.onclick = () =>{
         x.innerText = curr;
+        count++;
         if(curr == 'X') curr = 'O';
         else curr = 'X';
-        check();
-        if(variable == 'A')
         tur.innerText =` It's ${curr}'s Turn`;
-        else tur.innerText = `${variable} WON`;
-        // tur.style.animation ="roll 0.3s linear";
+        check();
         x.disabled = true;//After clicking cant change again
     }
 })
